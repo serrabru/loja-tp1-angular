3dd50997eb625ea47e6cc0b871e8b63ddf7248fe
serrabruna <brunaibserra@hotmail.com>
2025-08-22 22:14:24 -0300

    criação de header, footer e banner

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from './core/header/header';
+import { Footer } from './core/footer/footer';
+import { Banner } from './core/banner/banner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Footer, Banner],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('loja-tp1-angular');
+  
 }
